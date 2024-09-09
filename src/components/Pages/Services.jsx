7cffe0325c446403538d1ebca54a649a25763a3c
Abhilash CP <abhicplash@gmail.com>
2024-09-09 10:02:36 +0530

    ab service page done

--- src/components/Pages/Services.jsx
+++ src/components/Pages/Services.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./Services.css";
 import Layout from "../Layout/Layout";
+import { Arraylist } from "../../utils/data/categories";
 
 const Services = () => {
   return (
@@ -9,8 +10,27 @@ const Services = () => {
         <h2>Services</h2>
       </div>
       <div className="service-main">
-
-      <div>Services</div>
+        <h1 className="servicehead">We Provides</h1>
+        <p className="servicepara">
+          We offer high-performance tires for speed and agility. Durable
+          off-road tires for rugged terrain and adventure. Eco-friendly tires
+          for a smoother, fuel-efficient ride. Premium comfort tires for a quiet
+          and comfortable journey
+        </p>
+        <div className="servicesLst">
+          {Arraylist.map(tyre =>
+            <div className="tireCard">
+              <img src={tyre.image} alt="" />
+              <h3>
+                {tyre.name}
+              </h3>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Voluptatem, sed!
+              </p>
+            </div>
+          )}
+        </div>
       </div>
     </Layout>
   );
